bc002c46aa6928bf1de390f1cc805806c6a9c5ce
denisri <denis.riviere.gm@gmail.com>
2013-04-12 17:24:38 +0000

    renamed MyMesh and MyKDTree to constel::Mesh and constel::KDTree
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/connectomist/constellation-private/trunk@54317 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- src/library/tildefs.h
+++ src/library/tildefs.h
@@ -4,11 +4,11 @@
 #include <cathier/aims_wrap.h>
 #include <cathier/kdtree.h>
 
-namespace constellation
+namespace constel
 {
 
-  typedef til::Mesh_N MyMesh;
-  typedef til::KDTree<std::size_t, til::MeshTraits<MyMesh>::VertexCollection> MyKDTree;
+  typedef til::Mesh_N Mesh;
+  typedef til::KDTree<std::size_t, til::MeshTraits<Mesh>::VertexCollection> KDTree;
 
 }
 
