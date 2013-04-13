e2dd0c06ff7323d80c8a8c3377e05b8a1de0316c
denisri <denis.riviere.gm@gmail.com>
2013-04-13 15:13:34 +0000

    moved one more header from perso/roca
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/connectomist/constellation-private/trunk@54353 613412ce-1361-4b7a-93bf-a3c75af1bae4

---
+++ src/library/connectivities.h
@@ -0,0 +1,26 @@
+#ifndef CONSTELLATION_CONNECTIVITIES_H
+#define CONSTELLATION_CONNECTIVITIES_H
+
+#include <cathier/SparseVector.h>
+#include <vector>
+
+namespace constel
+{
+
+  typedef til::SparseVector<double> Connectivity;
+  typedef std::vector< Connectivity > Connectivities;
+  typedef std::vector<std::vector<std::size_t> > CNeighborhoods;
+  typedef std::vector< std::pair<std::size_t, double> > QuickMap;
+  typedef std::vector<QuickMap> Connection;
+  typedef std::vector<Connection> BundleConnections;
+  typedef boost::shared_ptr<BundleConnections> BundleConnections_shared_ptr;
+  typedef std::vector<uint> ConnectionsLength;
+  typedef std::vector<float> ConnectionsFloatLength;
+  typedef boost::shared_ptr<ConnectionsLength> ConnectionsLength_shared_ptr;
+  typedef std::vector<std::set<uint> > PolygonsByVertexIndex;
+  typedef std::vector<PolygonsByVertexIndex> PolygonsByVertexIndex_collection;
+
+}
+
+#endif
+
