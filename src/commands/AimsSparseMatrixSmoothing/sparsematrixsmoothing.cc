fc661b8c1ecdd7823b7c529e859f055060718a16
denisri <denis.riviere.gm@gmail.com>
2013-04-15 15:17:58 +0000

    moving AimsSparseMatrixSmoothing commandline to constellation
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/connectomist/constellation-private/trunk@55138 613412ce-1361-4b7a-93bf-a3c75af1bae4

---
+++ src/commands/AimsSparseMatrixSmoothing/sparsematrixsmoothing.cc
@@ -0,0 +1,53 @@
+#include <constellation/sparseMatrixSmoothing.h>
+#include <aims/getopt/getopt2.h>
+
+using namespace aims;
+using namespace carto;
+using namespace std;
+using namespace constel;
+
+int main( int argc, const char** argv )
+{
+  string matFilename;
+  Reader<AimsSurfaceTriangle> inMeshAimsR;
+  string outmatFilename;
+  double sigma;
+  double threshold = 0.0001;
+  bool gaussian = false;
+
+  AimsApplication app( argc, argv, "Sparse matrix smoothing using heat diffusion, with the geometry of a mesh. Can typically be used for connectivity matrix smoothing. Smoothing is applied for each line." );
+  app.addOption( matFilename, "-i", "input sparse matrix" );
+  app.addOption( inMeshAimsR, "-m", "input mesh" );
+  app.addOption( outmatFilename, "-o", "output sparse matrix" );
+  app.addOption( sigma, "-s", "sigma equivalent of the smoothing" );
+  app.addOption( threshold, "-t", "threshold smoothing coefficients under this value (default: 0.0001)", true );
+  app.addOption( gaussian, "-g",
+    "use gaussian smoothing. Default is heat diffusion", true );
+
+  try
+  {
+    app.initialize();
+
+    AimsSurfaceTriangle mesh;
+    inMeshAimsR.read( mesh );
+    SparseMatrix matrix;
+    matrix.read( matFilename );
+
+    if( gaussian )
+      sparseMatrixGaussianSmoothing( matrix, mesh, sigma, threshold );
+    else
+      sparseMatrixDiffusionSmoothing( matrix, mesh, threshold, sigma );
+    matrix.write( outmatFilename );
+
+    return EXIT_SUCCESS;
+  }
+  catch( user_interruption & )
+  {
+  }
+  catch( exception & e )
+  {
+    cerr << e.what() << endl;
+  }
+  return EXIT_FAILURE;
+}
+
---
+++ src/commands/AimsSparseMatrixSmoothing/sparsematrixsmoothing.pro
@@ -0,0 +1,3 @@
+TEMPLATE = app
+TARGET = AimsSparseMatrixSmoothing
+SOURCES = sparsematrixsmoothing.cc
