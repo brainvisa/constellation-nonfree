7b8c7feb328612c0986b0b9aad117ff7e0bb54cf
denisri <denis.riviere.gm@gmail.com>
2013-04-15 15:26:14 +0000

    improved help
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/connectomist/constellation-private/trunk@55141 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- src/commands/AimsSparseMatrixSmoothing/sparsematrixsmoothing.cc
+++ src/commands/AimsSparseMatrixSmoothing/sparsematrixsmoothing.cc
@@ -15,12 +15,14 @@ int main( int argc, const char** argv )
   double threshold = 0.0001;
   bool gaussian = false;
 
-  AimsApplication app( argc, argv, "Sparse matrix smoothing using heat diffusion, with the geometry of a mesh. Can typically be used for connectivity matrix smoothing. Smoothing is applied for each line." );
+  AimsApplication app( argc, argv, "Sparse matrix smoothing using heat diffusion or gaussian smoothing, with the geometry of a mesh. Can typically be used for connectivity matrix smoothing. Smoothing is applied for each line, each line is a texture for the mesh." );
   app.addOption( matFilename, "-i", "input sparse matrix" );
   app.addOption( inMeshAimsR, "-m", "input mesh" );
   app.addOption( outmatFilename, "-o", "output sparse matrix" );
   app.addOption( sigma, "-s", "sigma equivalent of the smoothing" );
-  app.addOption( threshold, "-t", "threshold smoothing coefficients under this value (default: 0.0001)", true );
+  app.addOption( threshold, "-t",
+    "threshold smoothing coefficients under this value (default: 0.0001)",
+    true );
   app.addOption( gaussian, "-g",
     "use gaussian smoothing. Default is heat diffusion", true );
 
