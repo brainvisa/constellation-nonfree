aff65112687070ca6300fe6ca4ad1aec755c54d7
lefranc <lefranc@bioproj.invalid>
2013-07-04 08:56:43 +0000

    add new functions: - findWrongLabels() - changeLabelOfWrongLabelVertices() - cleanGyriTexture()
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/connectomist/constellation-private/trunk@56877 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- python/constel/lib/texturetools.py
+++ python/constel/lib/texturetools.py
@@ -1,34 +1,33 @@
 #!/usr/bin/env python
+import soma.aims.texturetools as MTT
 from soma import aims
 import numpy
 
-
-def removeLabelsFromTexture(tex, labels_list):
+def removeLabelsFromTexture( tex, labels_list ):
   """
   inputs:
           tex: labeled texture between 1 and labelsNb, background = 0, aimsTimeTexture_S16
           labels_list: list of labels to remove        
   output:
-          out_tex:
-                labeled texture without region of labels_list, and renumbered between 1 and NewLabelsNb (= LabelsNb - len(labels_list))
+          out_tex: labeled texture without region of labels_list, and renumbered between 1 and NewLabelsNb (= LabelsNb - len(labels_list))
   """
   out_tex = aims.TimeTexture_S16()
-  vertex_nb = int(tex.nItem())
+  vertex_nb = int( tex.nItem() )
   tex_ar = tex[0].arraydata()
   out_tex[0].assign( tex_ar )
   labels_nb = tex_ar.max()
-  removedLabels_nb = len(labels_list)
+  removedLabels_nb = len( labels_list )
   outtex_ar = out_tex[0].arraydata()
   for label in labels_list:
     outtex_ar[outtex_ar==label]=0
-  outtex_kept_labels = numpy.unique(outtex_ar)
+  outtex_kept_labels = numpy.unique( outtex_ar )
   outtex_kept_labels_list = outtex_kept_labels.tolist()
   if outtex_kept_labels_list.count(0) != 0:
     outtex_kept_labels_list.remove(0)
-  for i in xrange(len(outtex_kept_labels_list)):
+  for i in xrange( len(outtex_kept_labels_list) ):
     current_label = outtex_kept_labels_list[i]
     print "current label:", current_label, " new:", str(i+1)
-    outtex_ar[tex_ar==current_label] = i+1
+    outtex_ar[ tex_ar==current_label ] = i+1
   return out_tex
 
 
@@ -53,3 +52,85 @@ def average_texture_labels( output, inputs ):
   otex[0].assign( maj )
 
   aims.write( otex, output )
+
+
+def findWrongLabels( mesh, gyriTex ):
+  """
+  inputs:
+         mesh
+         gyriTex: gyri texture
+  outputs:
+         wrong_labels: list of wrong labels
+         
+         [ cctex: connectedComponentTex: aimsTimeTexture_S16, time step = LabelsNb, for each time step (label in the tex), texture of the connected components corresponding to this label (background = -1, and connected components = values between 1 and ccNb)
+         areas_measures = python dictionary, areas_measures[label] = [16.5, 6.0] (numpy array) if label (in tex) has two connected Components 1 and 2 with area = 16.5 and 6.0 respectively, areas are in square mm ]
+  """
+  meshNeighborsVector = aims.SurfaceManip.surfaceNeighbours( mesh )
+  cctex, areas_measures = MTT.connectedComponents( mesh, gyriTex, areas_mode = 1 )
+  wrong_labels = []
+  for label in areas_measures.keys():
+    if areas_measures[ label ].size != 1:
+      wrong_labels.append( label )
+  return wrong_labels
+
+
+def changeLabelOfWrongLabelVertices( cclabel, label, gyriTex, meshNeighborsVector, cctexLabel_ar ):
+  """
+  inputs:
+          cclabel: label of connected component in cctexLabel_ar
+          label: label of associated vertices in gyri texture
+          gyriTex: gyri texture
+          meshNeighborsVector : aims.SurfaceManip.surfaceNeighbours(mesh) (mesh = white mesh associated to gyriTex)
+          cctexLabel_ar : cctex[label-1].arraydata()
+  outputs:
+          gyriTex: new gyriTex texture
+          winner_label
+  """
+  indexes=np.where( cctexLabel_ar == cclabel )[0]
+  neighbourLabels = []
+  print 'Nb of wrong indexes: ', indexes.size
+  for i in indexes:
+    for n in meshNeighborsVector[i]:
+      n_label = gyriTex[0][n]
+      if n_label != label:
+        neighbourLabels.append( n_label )
+  neighLabels_list = np.unique( neighbourLabels )
+  maxCount = 0
+  winnner_label = -1
+  for neighLabel in neighLabels_list:
+    neighLabelCount = neighbourLabels.count( neighLabel )
+    if neighLabelCount > maxCount:
+      print 'neighbourLabels.count( neighLabel ): ', neighbourLabels.count( neighLabel )
+      winnner_label = neighLabel
+      maxCount = neighLabelCount
+  for i in indexes:
+    gyriTex[0][i] = winnner_label
+  return gyriTex, winnner_label
+
+
+def cleanGyriTexture( mesh, gyriTex ):
+  """
+  inputs:
+         mesh
+         gyriTex: gyri texture
+  outputs:
+         gyriTex: new gyri texture
+
+         [ cctex: connectedComponentTex: aimsTimeTexture_S16, time step = LabelsNb, for each time step (label in the tex), texture of the connected components corresponding to this label (background = -1, and connected components = values between 1 and ccNb)
+         areas_measures = python dictionary, areas_measures[label] = [16.5, 6.0] (numpy array) if label (in tex) has two connected Components 1 and 2 with area = 16.5 and 6.0 respectively, areas are in square mm ]
+  """
+  meshNeighborsVector = aims.SurfaceManip.surfaceNeighbours( mesh )
+  print 'Mesh neighbors vector: ', meshNeighborsVector
+  cctex, areas_measures = MTT.connectedComponents( mesh, gyriTex, areas_mode = 1 )
+  wrong_labels = []
+  for label in areas_measures.keys():
+    if areas_measures[ label ].size != 1:
+      wrong_labels.append( label )
+  for label in wrong_labels:
+    cctexLabel_ar = cctex[ label-1 ].arraydata()
+    areas_measures_cc = areas_measures[ label ]
+    cc_nb = areas_measures_cc.size
+    for l in range( 1, cc_nb ):
+      gyriTex, win = changeLabelOfWrongLabelVertices( l+1, label, gyriTex, meshNeighborsVector, cctexLabel_ar )
+      print "Changed in ", win
+  return gyriTex
