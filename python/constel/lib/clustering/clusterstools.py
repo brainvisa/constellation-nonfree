a5b45274df906e79fda14d9465e8f2688637e2b0
lefranc <lefranc@bioproj.invalid>
2013-11-04 16:58:08 +0000

    Add functions for the clustering:   - getCenters()   - wcDist() (for gap statistic)
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/connectomist/constellation-private/trunk@59160 613412ce-1361-4b7a-93bf-a3c75af1bae4

---
+++ python/constel/lib/clustering/clusterstools.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+def getCenters(clust, K):
+  i = 0
+  centers = np.array([])
+  while (centers.size < K):
+    c = clust[i]
+    if ((np.where(centers == c)[0]).size == 0):
+      centers = np.append(centers, array([int(c)]))
+    i += 1
+  return centers
+
+def wcDist(distance, clusterid, K, fact):
+  centers = getCenters(clusterid, K)
+  W = zeros(K)
+  addW = 0.0
+  for l in range(K):
+    sum = 0.0
+    centerid = np.where(clusterid == centers[l])[0]
+    for i in centerid:
+      for j in centerid:
+        if (i < j):
+          sum += (distance[j][i] ** fact)
+        elif (j < i):
+          sum += (distance[i][j] ** fact)
+    W[l] = sum / float(2.0 * centerid.size)
+    addW += W[l]
+  return addW
