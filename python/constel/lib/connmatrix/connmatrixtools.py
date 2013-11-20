5fb63b7968f2da61a7d86ba85ae84e521a07564d
lefranc <lefranc@bioproj.invalid>
2013-11-20 14:52:31 +0000

    orderDataMatrix() and  euclidianDistanceMatrix() --> from roca to constel
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/connectomist/constellation-private/trunk@59592 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- python/constel/lib/connmatrix/connmatrixtools.py
+++ python/constel/lib/connmatrix/connmatrixtools.py
@@ -1,4 +1,4 @@
-
+#!/usr/bin/env python
 from soma import aims
 import numpy as np
 
@@ -24,4 +24,49 @@ def permutationResampling(feat):
   perm = np.random.permutation(feat[:, 0].reshape((Nsamples, 1)))
   for f in range(1, Ndim):
     perm = np.hstack((perm, np.random.permutation(feat[:, f].reshape((Nsamples, 1))) ))
-  return perm    
+  return perm
+  
+def orderDataMatrix(mat, labels):
+  """
+  inputs:
+            mat: matrix of observations, shape = (n,p), n observations, p attributes
+            labels: numpy array of shape (n) corresponding to the labels of observations
+            
+  output:   order_mat: ordered matrix, of shape (n,p)
+            labels: ordered labels
+  """
+  (n,p)=mat.shape
+  obs_nb = labels.size
+  if n!=obs_nb:
+    raise exceptions.ValueError("matrix dimensions and labels size are not compatible")
+  labels_argsort = labels.argsort()
+  order_mat = N.zeros((n,p),dtype = N.float32)
+  for i in xrange(n):
+    order_mat[i,:]=mat[labels_argsort[i],:]
+  sortLabels= labels.copy()
+  sortLabels.sort()
+  return order_mat, sortLabels
+
+def euclidianDistance(v1,v2):
+  """
+  input:
+          v1, v2: two vectors, shape (1,p) (numpy arrays)
+  output:
+          dist(v1,v2)
+  """
+  dist = ((v1-v2)**2).sum()
+  return N.sqrt(dist)
+   
+def euclidianDistanceMatrix(matrix):
+  (n,p)=matrix.shape
+  euclidian_dist_matrix = N.zeros((n,n), dtype = N.float)
+  for i in xrange(n):
+    v1 = matrix[i]
+    dist_value = 0
+    euclidian_dist_matrix[i][i]=0
+    for j in xrange(0,i):
+      v2 = matrix[j]
+      dist_value = euclidianDistance(v1,v2)
+      euclidian_dist_matrix[i][j]= dist_value
+      euclidian_dist_matrix[j][i]= dist_value
+  return euclidian_dist_matrix
