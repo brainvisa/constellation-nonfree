7967f364263a7ea26c5792fe7c7b7673dfcaa880
lefranc <lefranc@bioproj.invalid>
2014-05-16 12:13:31 +0000

    from roca to constel
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/connectomist/constellation-private/trunk@62133 613412ce-1361-4b7a-93bf-a3c75af1bae4

---
+++ src/library/mergeMeshAndROIs.h
@@ -0,0 +1,22 @@
+#ifndef CONSTELLATION_FUSIONMESHROIS_TO_TEXTURE_H
+#define CONSTELLATION_FUSIONMESHROIS_TO_TEXTURE_H
+
+#include <aims/mesh/surface.h>
+#include <aims/mesh/texture.h>
+#include <aims/resampling/motion.h>
+
+class Graph;
+
+namespace constel {
+
+  TimeTexture<short> * fusionMeshROIs_to_Texture_S16(const AimsSurfaceTriangle & inAimsMesh, const Graph & ROI_graph, float distthresh, std::string vertex_property = "aims_roi");
+
+  TimeTexture<short> * fusionMeshFoldGraph_to_Texture(const AimsSurfaceTriangle & inAimsMesh, const Graph & ROI_graph, float distthresh, std::string vertex_property = "aims_roi", std::string label_name = "roi_label");
+
+  Graph * texMeshAndBundles_to_BundlesGraph( const AimsSurfaceTriangle & inAimsMesh, carto::rc_ptr<TimeTexture<short> > tex, std::string namesMode, std::string bundlesFile_name, Motion motion, const std::string & BundlesNamesfile_name = std::string() );
+
+  Graph * texMeshAndBundles_to_BundlesGraph_WithIntersectionComputing( const AimsSurfaceTriangle & inAimsMesh, const TimeTexture<short> & tex, std::string bundlesFile_name, Motion motion, std::string BundlesNamesfile_name);
+
+} // namespace constel
+
+#endif // ifndef CONSTELLATION_FUSIONMESHROIS_TO_TEXTURE_H
