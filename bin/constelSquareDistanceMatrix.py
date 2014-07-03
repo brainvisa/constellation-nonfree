35bf4b24ff3f6feae9d27a1f3ba75a6728a989a5
lefranc <lefranc@bioproj.invalid>
2014-07-03 15:38:33 +0000

    new command to compute the square distance matrix in parallel (but is definitely incomplete and must be ameliorated)
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/connectomist/constellation-private/trunk@62830 613412ce-1361-4b7a-93bf-a3c75af1bae4

---
+++ bin/constelSquareDistanceMatrix.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+
+# python system modules
+import numpy as np
+import optparse
+import sys
+
+# scipy
+from scipy.spatial.distance import pdist
+
+def parseOpts(argv):
+    desc = """Generate a square distance matrix in parallel."""
+    parser = optparse.OptionParser(desc)
+    parser.add_option('-m', '--matrix', dest='matrix',
+                      help='input reduced connectivity matrix')   
+    parser.add_option('-s', '--startiter', dest='starting_iteration', 
+                      type='int', help='starting of iteration')                   
+    parser.add_option('-t', '--nbiter', dest='nb_iteration', type='int',
+                      help='number of iteration by job')                    
+    parser.add_option('-k', '--idk', dest='id_distmat', type='int',
+                      help='position in the distance matrix')
+    parser.add_option('-i', '--input', dest='indir', metavar='FILE',
+                      help='file to construct the square distance matrix')
+    parser.add_option('-d', '--method', dest='method', default='scipy',
+                      help='two methods to generate the distance matrix:'
+                           '(1) scipy --> risk of Memory Error,' 
+                           '(2) perso --> partition of distance matrix,'
+                           '[default: %default]')
+    return parser, parser.parse_args(argv)
+
+def main():
+    parser, (options, args) = parseOpts(sys.argv)
+    
+    # matrix loading
+    matrix = np.load(options.matrix)
+    m, n = matrix.shape
+
+    #######################################################################
+    #        Two methods to generate the distance matrix
+    #          (1) scipy: generate all matrix, risk of Memory Error 
+    #          (2) perso: generate a portion of matrix (required)
+    #######################################################################
+
+    if options.method == 'scipy':
+        # generate a triangular distance matrix as vector, with scipy
+        # computes the Euclidean distance
+        distance_matrix = pdist(matrix, metric='euclidean')
+    else:
+        # generate the upper triangular distance matrix bounded by
+        # starting_iteration and  max iteration required
+        distance_matrix = []
+        for i in xrange(0, (m-1)):
+            for j in xrange(i + 1, m):
+                distance_matrix.append(
+                    np.sqrt(sum((matrix[i, :] - matrix[j, :]) * 
+                                (matrix[i, :] - matrix[j, :]))))
+            
+    # open the file to construct the square distance matrix    
+    f = open(options.indir, 'r+')
+    
+    # initialization of the square distance matrix
+    square_distance_matrix = np.zeros((m, m), 'd')
+    
+    #######################################################################
+    # complete the square distance matrix bounded by starting_iteration and 
+    # max iteration required
+    #######################################################################
+    
+    # upper/right triangular matrix
+    k = int(options.id_distmat)
+    for i in xrange(options.starting_iteration, options.nb_iteration + 
+                    options.starting_iteration):
+        for j in xrange(i + 1, m):
+            square_distance_matrix[i][j] = distance_matrix[k]
+            k += 1
+            
+    # lower/left triangular matrix
+    kk = 0
+    for jj in xrange(m):
+        for ii in xrange(jj, m):
+            print kk
+            if jj == ii:
+                pass
+            elif ii <= (options.starting_iteration - 1):
+                kk += 1
+            else:
+                square_distance_matrix[ii][jj] = distance_matrix[kk]
+                kk += 1
+    
+    # initial position to complete the square distance matrix in the file       
+    f.seek(options.starting_iteration * m)
+    
+    # write the square distance matrix bounded by starting_iteration and 
+    # max iteration required
+    f.write(square_distance_matrix[options.starting_iteration:options.nb_iteration + 
+                    options.starting_iteration][:].ravel())
+    print square_distance_matrix[options.starting_iteration:options.nb_iteration + 
+                    options.starting_iteration][:].ravel()
+
+    # remove the square distance matrix to free the memory
+    del square_distance_matrix
+    del distance_matrix
+    f.close()
+                    
+if __name__ == "__main__":
+    main()
---
+++ bin/constelSquareDistanceMatrix_workflow.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+
+# python system modules
+import numpy as np
+import optparse
+import pickle
+import sys
+
+# soma_workflow
+import soma_workflow.client as swf
+
+# soma
+from soma.path import find_in_path
+
+def parseOpts(argv):
+    desc = """Workflow to generate the square distance matrix in parallel"""
+    parser = optparse.OptionParser(desc)
+    parser.add_option('-m', '--matrix', dest='matrix',
+                      help='input reduced connectivity matrix')                
+    parser.add_option('-j', '--jobs', dest='jobs', type='int',
+                      help='number of jobs')
+    parser.add_option('-i', '--input', dest='indir', metavar='FILE',
+                      help='file to construct the square distance matrix')                 
+    parser.add_option('-o', '--output', dest='output', metavar='FILE',
+                      help='workflow file')
+    parser.add_option('-d', '--method', dest='method', default='scipy',
+                      help='two methods to generate the distance matrix:'
+                           '(1) scipy --> risk of Memory Error,' 
+                           '(2) perso --> partition of distance matrix,'
+                           '[default: %default]')
+    return parser, parser.parse_args(argv)             
+
+def main():
+    parser, (options, args) = parseOpts(sys.argv)
+    
+    # matrix loading
+    matrix = np.load(options.matrix)
+    m, n = matrix.shape
+    
+    # create the file to receive the elements of the square distance matrix
+    f = open(options.indir, 'w')
+    f.seek(m)
+    f.close()
+
+    # determines the numbers of iterations by job
+    jobs = []
+    nb_iteration = m / options.jobs
+    nb_add = m % options.jobs
+    list_iteration = [nb_iteration] * options.jobs
+
+    # hacks...
+    starting_iteration = 0
+    k = starting_iteration
+    x = m - 1
+    y = 1
+
+    # run command
+    for j in xrange(options.jobs):
+        # limit on the numbers of job
+        if j < nb_add:
+            list_iteration[j] += 1
+        cmd = ['python', find_in_path('constelSquareDistanceMatrix.py'),
+               '-m', options.matrix,
+               '-s', str(starting_iteration),
+               '-t', str(list_iteration[j]),
+               '-k', str(k),
+               '-i', options.indir,
+               '-d', options.method]
+        # parameters for each job    
+        job = swf.Job(command=cmd, name='job_%d' % j)
+        jobs.append(job)
+        
+        # definition of starting iteration, corresponding to matrix line
+        starting_iteration += list_iteration[j]
+        
+        # indice of k in triangular distance matrix as vector (hack...)
+        for i in xrange(list_iteration[j]):
+            k += x
+            x = (m - 1) - y
+            y += 1
+
+    # create workflow
+    workflow = swf.Workflow(jobs=jobs, name='square_distance_matrix')
+    pickle.dump(workflow, open(options.output, 'w'))
+    
+if __name__ == "__main__":
+    main()
