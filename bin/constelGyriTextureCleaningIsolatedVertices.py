fdaed548c6fff4ca57bbf11e3dbbd3f422449796
lefranc <lefranc@bioproj.invalid>
2014-08-08 12:51:44 +0000

    pep8
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/connectomist/constellation-private/trunk@63286 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- bin/constelGyriTextureCleaningIsolatedVertices.py
+++ bin/constelGyriTextureCleaningIsolatedVertices.py
@@ -1,45 +1,44 @@
 #!/usr/bin/env python
 from soma import aims
 import optparse
-import numpy
 import sys
+import constel.lib.texturetools as tt
 
 def validation():
-  try:
-    import constel
-  except:
-    raise ValidationError( 'constellation module is not here.' )
+    try:
+        import constel.lib.texturetools as tt
+    except:
+        raise ValidationError('constellation module is not here.')
 
-import constel.lib.texturetools as TT
-
-def parseOpts( argv ):
-  description = 'Threshold an aimsTimeTexture (with one time step only ! Warning !)'
-  parser = optparse.OptionParser( description )
-  parser.add_option('-i', '--itex', dest = 'tex',
-    metavar = 'FILE',
-    help = 'input gyri texture' )
-  parser.add_option('-m', '--mesh', dest = 'mesh',
-    metavar = 'FILE',
-    help = 'input mesh' )
-  parser.add_option('-o', '--otex', dest = 'otex',
-    metavar = 'FILE', action = 'store', default = None,
-    help = 'output gyri texture with one connected component per gyrus (default : don\'t store resulting texture)' )
-  return parser, parser.parse_args( argv )
+def parseOpts(argv):
+    description = 'Threshold an aimsTimeTexture' \
+                  '(Warning: with one time step only)'
+    parser = optparse.OptionParser(description)
+    parser.add_option('-i', '--itex', dest='tex', metavar='FILE',
+                      help='input gyri texture')
+    parser.add_option('-m', '--mesh', dest='mesh', metavar='FILE',
+                      help='input mesh' )
+    parser.add_option('-o', '--otex', dest='otex', metavar='FILE',
+                      action='store', default=None,
+                      help='output gyri texture with one connected component' \
+                           'per gyrus' \
+                           '(default : don\'t store resulting texture)')
+    return parser, parser.parse_args(argv)
 
 def main():
-  parser, ( options, args ) = parseOpts( sys.argv )
-  tex = aims.read( options.tex )
-  mesh = aims.read( options.mesh )
-  cont = True
-  count = 0
-  while cont and count < 10:
-    print "clean up number ", str( count+1 ), " :"
-    tex = TT.clean_gyri_texture( mesh, tex )
-    wrong_labels = TT.findWrongLabels( mesh, tex )
-    cont = False
-    count += 1
-    if len( wrong_labels ) > 0:
-      cont = True
-  aims.write( tex, options.otex )
+    parser, (options, args) = parseOpts(sys.argv)
+    tex = aims.read(options.tex)
+    mesh = aims.read(options.mesh)
+    cont = True
+    count = 0
+    while cont and count < 10:
+        print "clean up number ", str(count + 1), " :"
+        tex = tt.clean_gyri_texture(mesh, tex)
+        wrong_labels = tt.find_wrong_labels(mesh, tex)
+        cont = False
+        count += 1
+        if len(wrong_labels) > 0:
+            cont = True
+    aims.write(tex, options.otex)
 
 if __name__ == "__main__" : main()
