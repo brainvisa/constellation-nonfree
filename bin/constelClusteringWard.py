4c881c9a66b0a97d65afde3b162a14ff7f39fff5
lefranc <lefranc@bioproj.invalid>
2014-08-08 12:29:54 +0000

    improvements and code cleanup
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/connectomist/constellation-private/trunk@63278 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- bin/constelClusteringWard.py
+++ bin/constelClusteringWard.py
@@ -1,78 +1,83 @@
-#!/usr/bin/env python
+#! /usr/bin/env python
+#############################################################################
+# This software and supporting documentation are distributed by
+#      CEA/NeuroSpin, Batiment 145,
+#      91191 Gif-sur-Yvette cedex
+#      France
+# This software is governed by the CeCILL license version 2 under
+# French law and abiding by the rules of distribution of free software.
+# You can  use, modify and/or redistribute the software under the
+# terms of the CeCILL license version 2 as circulated by CEA, CNRS
+# and INRIA at the following URL "http://www.cecill.info".
+#############################################################################
 
 # python system modules
-from optparse import OptionParser
+import sys
 import numpy as np
-import system
+from optparse import OptionParser
 
 # soma
 from soma import aims
 
-# constel
+# constel modules
 from constel.lib.clustering.clusterstools import ward_method
 from constel.lib.texturetools import texture_time
 
+
 def parseOpts(argv):
-    description = 'Clustering Ward: one texture per subject'
+    description = "Clustering Ward: one texture per subject"
 
     parser = OptionParser(description)
 
-    parser.add_option('-k', '--kmax', dest='kmax', type='int',
-                      help='Number of clusters')
-    parser.add_option('-l', '--label', dest='patch', 
-                      help='Region of interest\'s, between 1 to parcelRegionsNb' )
-    parser.add_option('-x', '--matrix', dest='cmat',
+    parser.add_option(
+        '-k', '--kmax', dest='kmax', type='int', help='Number of clusters')
+    parser.add_option('-l', '--label', dest='patch',
+                      help='Region of interest\'s,' 
+                           'between 1 to parcelRegionsNb')
+    parser.add_option('-x', '--matrix', dest='cmat', 
                       help='input reduced connectivity matrix')
-    parser.add_option('-c', '--distmatrix', dest = 'dmat', 
-                      help = 'input distance matrix file')
-    parser.add_option('-m', '--avgmesh', dest = 'avg_mesh', 
-                      help = 'Input average mesh to the correct group')
+    parser.add_option(
+        '-c', '--distmatrix', dest='dmat', help='input distance matrix file')
+    parser.add_option('-m', '--avgmesh', dest='avg_mesh',
+                      help='Input average mesh to the correct group')
     parser.add_option('-g', '--gyritex', dest='gyri_tex', action='append', 
                       help='List of gyri segmentation (inputs)')
-    parser.add_option('-o', '--outputdir', dest='odir', 
-                      help='output directory')
-    parser.add_option('-t', '--textime', dest = 'tex_time', action='append',
-                      help='List of time texture (outputs)')
+    parser.add_option(
+        '-o', '--outputdir', dest='odir', help='output directory')
+    parser.add_option('-t', '--textime', dest='tex_time',
+                      action='append', help='List of time texture (outputs)')
 
     return parser, parser.parse_args(argv)
 
+
 def main():
     parser, (options, args) = parseOpts(sys.argv)
-    
-    # Load the distance matrix
+
+    # Load the reduced matrix
     matrix = aims.read(options.cmat)
     cmat = np.asarray(matrix)[:, :, 0, 0]
-    n, m = cmat.shape
-    dist_mat_file = np.load(options.dmat)
-    
+
     # Compute linkage
-    clusterid = ward_method(dist_mat_file, n, options.odir, options.kmax)
-    
-    count_vertex = 0
+    clusterid = ward_method(
+        options.dmat, cmat.shape[0], options.odir, options.kmax)
+
+    min_index = 0
     avg_mesh = aims.read(options.avg_mesh)
     nb_vertices = avg_mesh.vertex().size()
-    nb_tex = len(options.gyri_tex)
 
-    # generate all individual textures time 
-    num = 0
-    for subject in xrange(nb_tex):
-        tex = aims.read(options.gyri_tex[num])
+    # generate all individual textures time
+    for i in xrange(len(options.gyri_tex)):
+        tex = aims.read(options.gyri_tex[i])
         vertices_patch = []
-        for i in xrange(tex[0].nItem()):
-            if tex[0][i] == int(options.patch):
-                vertices_patch.append(i)
+        for j in xrange(tex[0].nItem()):
+            if tex[0][j] == int(options.patch):
+                vertices_patch.append(j)
         nb_vertex = len(vertices_patch)
-        min_index = count_vertex
-        max_index = count_vertex + nb_vertex
-        clusters = texture_time(options.kmax, 
-                                clusterid, 
-                                vertices_patch, 
-                                nb_vertices, 
-                                2,
-                                min_index, 
-                                max_index)
-        count_vertex += nb_vertex
-        aims.write(clusters, str(options.tex_time[num]))
-        num += 1
+        max_index = min_index + nb_vertex
+        clusters = texture_time(options.kmax, clusterid, vertices_patch, 
+                                nb_vertices, 2, min_index, max_index)
+        min_index += nb_vertex
+        aims.write(clusters, str(options.tex_time[i]))
 
-if __name__ == "__main__" : main()
+if __name__ == "__main__":
+    main()
