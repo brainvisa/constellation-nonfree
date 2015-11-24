e3f5898d7d3846d30ef5869fd8d37edea7079541
denisri <denis.riviere.gm@gmail.com>
2015-11-24 17:29:16 +0000

    merge
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/connectomist/constellation-private/trunk@70343 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- python/constel/lib/methods/squared_euclidean_distance_matrices.py
+++ python/constel/lib/methods/squared_euclidean_distance_matrices.py
@@ -115,7 +115,7 @@ def compute_SEDM(matrix, method=0):
             type_converters=scipy.weave.converters.blitz, compiler="gcc")
     return dist_mat
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     import timeit
     n = 8
     m = [50, 100, 200, 400, 800, 1600, 3200, 6400]
