0a7fbcdaa25b3654fdd97ec117a58b04b47ec688
lefranc <lefranc@bioproj.invalid>
2016-01-25 17:11:28 +0000

    statistical_tools.py: calculate a percentage connmatrix/connmatrixtools.py: compute the clusters/basins matrix
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/connectomist/constellation-private/branches/bug_fix@71190 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- python/constel/lib/statistical_tools.py
+++ python/constel/lib/statistical_tools.py
@@ -1,12 +1,62 @@
-#! /usr/bin/env python
+#!/usr/bin/env python
+###############################################################################
+# This software and supporting documentation are distributed by CEA/NeuroSpin,
+# Batiment 145, 91191 Gif-sur-Yvette cedex, France. This software is governed
+# by the CeCILL license version 2 under French law and abiding by the rules of
+# distribution of free software. You can  use, modify and/or redistribute the
+# software under the terms of the CeCILL license version 2 as circulated by
+# CEA, CNRS and INRIA at the following URL "http://www.cecill.info".
+###############################################################################
+
 """
-Smoothing technique: moving average.
+This script does the following:
+*
+*
+
+Main dependencies: PyAims library
+
+Author: Sandrine Lefranc, 2014
 """
-# Created on Thu Oct  9 10:30:34 2014
-# @author: sl236442
+
+#----------------------------Imports-------------------------------------------
+
+
+# system module
+import numpy
 
 # Matplotlib module
 import matplotlib.pyplot as plt
+
+
+#----------------------------Functions-----------------------------------------
+
+
+def calculate_percentage(matrix):
+    """Calculate a percentage on the rows of a matrix.
+
+    Parameters
+    ----------
+        matrix: numpy.array (mandatory)
+
+    Return
+    ------
+        percent_matrix: numpy.array
+    """
+    # tuple of array dimensions
+    ndmatrix = matrix.shape
+
+    # initialyze a matrix with the same size as input matrix
+    percent_matrix = numpy.zeros(
+        (ndmatrix[0], ndmatrix[1]), dtype=numpy.float32)
+
+    # work on the rows of the matrix
+    for i in range(ndmatrix[0]):
+        # each element of the rows is divided by total number
+        for idx, value in enumerate(matrix[i]):
+            percent = (value / sum(matrix[i])) * 100
+            percent_matrix[i][idx] = percent
+
+    return percent_matrix
 
 
 def sum_list(list_elements, start, end):
@@ -96,7 +146,6 @@ def moving_average(x, y, k):
     return xm, ym
 
 
-
 def overlap(x, y):
     i = 0
     j = 0
@@ -108,7 +157,7 @@ def overlap(x, y):
             j += 1
         elif x[i] < y[j]:
             i += 1
-        else: # x[i] == y[j]
+        else:  # x[i] == y[j]
             c += 1
             i += 1
             j += 1
