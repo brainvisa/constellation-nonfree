0a903382054f126d07b6d323db8526546363b5ae
lefranc <lefranc@bioproj.invalid>
2016-10-07 15:45:50 +0000

    transfert command and functions from constel to aims.
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/connectomist/constellation-private/branches/bug_fix@74880 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- python/constel/lib/utils/texturetools.py
+++ python/constel/lib/utils/texturetools.py
@@ -8,7 +8,6 @@
 # CEA, CNRS and INRIA at the following URL "http://www.cecill.info".
 ###############################################################################
 
-import soma.aims.texturetools as MTT
 from soma import aims
 import numpy as np
 
@@ -119,111 +118,6 @@ def remove_labels(tex, labels):
         print "current label:", current_label, " new:", str(i + 1)
         otex_ar[tex_ar == current_label] = i + 1
     return otex
-
-
-def change_wrong_labels(cc_label, label, gyri_tex, mesh_neighbors_vector,
-                        cc_tex_label):
-    """After a study of its neighbors, wrong label is replaced by the
-    correct number.
-
-    Parameters
-    ----------
-    cc_label: label of connected component in cc_tex_label
-    label: label of associated vertices in gyri texture
-    gyri_tex (aims time texture S16): gyri texture
-    mesh_neighbors_vector : aims.SurfaceManip.surfaceNeighbours(mesh)
-    cc_tex_label : texture representing connected components of label
-
-    Returns
-    -------
-    gyri_tex (aims time texture S16): new gyri_tex texture,
-                                      without isolated vertex.
-    winner_label: the correct number.
-    """
-    indexes = np.where(cc_tex_label == cc_label)[0]
-    neighbor_labels = []
-    print 'Nb of wrong indexes: ', indexes.size
-    for i in indexes:
-        for n in mesh_neighbors_vector[i]:
-            n_label = gyri_tex[0][n]
-            if n_label != label:
-                neighbor_labels.append(n_label)
-    v_labels = np.unique(neighbor_labels)
-    max_count = 0
-    winnner_label = -1
-    for l in v_labels:
-        nb_v_labels = neighbor_labels.count(l)
-        if nb_v_labels > max_count:
-            print 'Number of neighbor labels: ', nb_v_labels, 'for', l
-            winnner_label = l
-            max_count = nb_v_labels
-    for i in indexes:
-        gyri_tex[0][i] = winnner_label
-    return gyri_tex, winnner_label
-
-
-def find_wrong_labels(mesh, gyriTex):
-    """
-
-    Parameters
-    ----------
-    mesh:
-    gyriTex: gyri texture
-
-    Returns
-    -------
-    wrong_labels: list of wrong labels
-        [cctex: connectedComponentTex: aimsTimeTexture_S16,
-        time step = LabelsNb, for each time step (label in the tex),
-        texture of the connected components corresponding to this label
-        (background = -1, and connected components = values between 1 and ccNb)
-        areas_measures = python dictionary,
-        areas_measures[label] = [16.5, 6.0] (numpy array)
-        if label (in tex) has two connected Components 1 and 2 with
-        area = 16.5 and 6.0 respectively, areas are in square mm]
-    """
-
-    meshNeighborsVector = aims.SurfaceManip.surfaceNeighbours(mesh)
-    cctex, areas_measures = MTT.connectedComponents(
-        mesh, gyriTex, areas_mode=1)
-    wrong_labels = []
-    for label in areas_measures.keys():
-        if areas_measures[label].size != 1:
-            wrong_labels.append(label)
-    return wrong_labels
-
-
-def clean_gyri_texture(mesh, gyri_tex):
-    """Cleaning a gyri texture by using connected components.
-
-    Parameters
-    ----------
-    mesh (aims time surface):
-        white mesh associated to gyri_tex
-    gyri_tex (aims time texture S16):
-        gyri texture as full FreeSurfer parcellation.
-    Return
-    ------
-    gyri_tex (aims time texture S16):
-        new gyri texture, without isolated vertex.
-    """
-    # get a list of neighbouring nodes from a surface mesh
-    mesh_neighbors_vector = aims.SurfaceManip.surfaceNeighbours(mesh)
-
-    cc_tex, areas_measures = MTT.connectedComponents(
-        mesh, gyri_tex, areas_mode=1)
-    wrong_labels = []
-    for label in areas_measures.keys():
-        if areas_measures[label].size != 1:
-            wrong_labels.append(label)
-    for label in wrong_labels:
-        cc_tex_label = cc_tex[label - 1].arraydata()
-        areas_measures_cc = areas_measures[label]
-        cc_nb = areas_measures_cc.size
-        for l in range(1, cc_nb):
-            gyri_tex, win = change_wrong_labels(
-                l + 1, label, gyri_tex, mesh_neighbors_vector, cc_tex_label)
-    return gyri_tex
 
 
 def texture_time(k_max, clusters_id, vertices_patch, vertices_mesh, mode,
