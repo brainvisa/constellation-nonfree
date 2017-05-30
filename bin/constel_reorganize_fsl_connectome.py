d4e3ff0a4f84abe199439b998bfdcee59568d25c
lefranc <lefranc@bioproj.invalid>
2017-05-30 11:34:18 +0000

    constel_reorganize_fsl_connectome.py: improvements + doc
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/constellation/constellation-private/branches/bug_fix@77771 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- bin/constel_reorganize_fsl_connectome.py
+++ bin/constel_reorganize_fsl_connectome.py
@@ -27,11 +27,19 @@ parser = argparse.ArgumentParser(description=description)
 parser.add_argument(
     'vertex_labels',
     type=str,
-    help='List of labels for left or right hemisphere.')
+    help='List of labels for both hemispheres.')
 parser.add_argument(
     'fdt_matrix',
     type=str,
     help='Vertex to vertex connectivity.')
+parser.add_argument(
+    'coords',
+    type=str,
+    help='Coords to vertex connectivity.')
+parser.add_argument(
+    'label',
+    type=int,
+    help='Label.')
 parser.add_argument(
     'outdir',
     type=str,
@@ -45,6 +53,8 @@ args = parser.parse_args()
 vlabels = args.vertex_labels
 fdt_matrix = args.fdt_matrix
 outdir = args.outdir
+label = args.label
+coords = args.coords
 
 if not os.path.isdir(outdir):
     os.makedirs(outdir)
@@ -53,24 +63,38 @@ if not os.path.isdir(outdir):
 aims_vlabels = aims.read(vlabels)
 np_vlabels = aims_vlabels[0].arraydata()
 
-# list of unique labels
-labelnames = numpy.unique(np_vlabels)
+#
+idx = numpy.where(np_vlabels == label)[0]
+inv_idx = numpy.zeros((len(np_vlabels)), dtype=int) - 1
+inv_idx[idx] = range(len(idx))
 
-for label in labelnames:
-    idx = numpy.where(np_vlabels == label)[0]
+# Initialize the matrix dimension
+connectome = numpy.zeros((len(idx), len(np_vlabels)))
 
-    connectome = numpy.zeros((len(idx), len(np_vlabels) * 2), dtype=int)
+coords_list = []
+nv_hemi = len(np_vlabels) / 2
+with open(coords, 'r') as f:
+    for line in f:
+        elements = line.strip().split()
+        hemi, vertex = int(elements[-2]), int(elements[-1])
+        coords_list.append(nv_hemi * hemi + vertex)
 
-    with open(fdt_matrix, 'r') as f:
-        for line in f:
-            v1_idx, v2_idx, nb_connections = map(float, line.strip().split())
-            if v1_idx in idx:
-                itemindex = numpy.where(idx == v1_idx)[0]
-                connectome[numpy.int(itemindex[0]),
-                           numpy.int(v2_idx)] = nb_connections
-    vol = aims.Volume(connectome.astype(float))
-    smat = aims.SparseOrDenseMatrix()
-    smat.setMatrix(vol)
-    output_connectome = os.path.join(outdir,
-                                     'connectome_label' + str(label) + '.imas')
-    aims.write(smat, output_connectome)
+with open(fdt_matrix, 'r') as f:
+    for line in f:
+        elements = line.strip().split()
+        v1_idx, v2_idx, nb_connections \
+            = (coords_list[int(elements[0]) - 1],
+               coords_list[int(elements[1]) - 1],
+               float(elements[2]))
+        if inv_idx[v1_idx] >= 0:
+            connectome[inv_idx[v1_idx], v2_idx] += nb_connections
+        if inv_idx[v2_idx] >= 0:
+            connectome[inv_idx[v2_idx], v1_idx] += nb_connections
+
+# Write the matrix as Volume on the disk
+vol = aims.Volume(connectome.astype(float))
+smat = aims.SparseOrDenseMatrix()
+smat.setMatrix(vol)
+output_connectome = os.path.join(outdir,
+                                 'connectome_label' + str(label) + '.imas')
+aims.write(smat, output_connectome)
