a034babfdb940c006dd5200715775991afa504b4
denisri <denis.riviere.gm@gmail.com>
2017-10-24 09:50:44 +0000

    merge
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/constellation/constellation-private/trunk@79576 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- bin/constel_calculate_asw.py
+++ bin/constel_calculate_asw.py
@@ -20,9 +20,12 @@ import json
 import numpy
 import argparse
 import textwrap
+
+# matplotlib. Set it to a non-graphical PDF backend
+import matplotlib
+matplotlib.use("pdf")
 from matplotlib import pyplot
 from matplotlib.backends.backend_pdf import PdfPages
-
 
 # soma module
 from soma import aims
@@ -45,8 +48,8 @@ Calculate the average silhouette width in order to find the optimal number of
 clusters.
 """
 
-def mylist(string):
-    return json.loads(string)
+#def mylist(string):
+    #return json.loads(string)
 
 
 def parse_args(argv):
@@ -59,7 +62,7 @@ def parse_args(argv):
 
     # adding arguments
     parser.add_argument(
-        "matrices", type=mylist,
+        "matrices", type=str, nargs="+",
         help="List of the individual reduced matrix.")
     parser.add_argument(
         "kmax", type=int,
@@ -68,20 +71,52 @@ def parse_args(argv):
         "ofile", type=str,
         help="The results will write in this PDF file.")
     parser.add_argument(
-        "-s", "--scaley", type=mylist, dest="ybound",
+        "-s", "--scaley", type=float, action="append", dest="ybound",
+        default=[0., 1.],
         help="Do the scale given on the axe Y.")
     parser.add_argument(
         "-r", "--removek2", action="store_true", dest="ignore_k2",
-        help="Ignore K=2 in the research of the optimal number of clusters.")
+        help="Ignore K=2 in the research of the optimal number of clusters. "
+        "Equivalent to --kmintype min --kmin 3.")
     parser.add_argument(
         "-c", "--scores", action="store_true", dest="save_scores",
         help="Write the ASW values (.npy) on the disk.")
+    parser.add_argument(
+        "-m", "--kmintype", dest="kmin_type", default="",
+        help="Selection method for minimum K. Possible values are "
+        "\"none\" (default), \"min\": hard-coded min "
+        "(see --kmin parameter), "
+        "\"avg_pts\": corresponds to an average number of points "
+        "(kmin = region pts / max_avg_pts, see --max_avg_pts)")
+    parser.add_argument(
+        "--kmin", type=int, default=0,
+        help="minimum number of clusters taken into account to search for the "
+        "optimum. Used when option --kmintype is \"min\".")
+    parser.add_argument(
+        "--max_avg_pts", type=float,
+        help="Max. average number of points for min number of "
+        "clusters. Used when opton --kmintype is \"avg_pts\". "
+        "Ideally it should rather be a surface but we would require "
+        "to input the mesh as additional argument for that to get "
+        "the surface, and this should be enough.")
 
     # parsing arguments
-    return parser, parser.parse_args(argv)
+    args = parser.parse_args(argv)
+    if args.ignore_k2:
+        if args.kmin_type not in ("", "min") or args.kmin not in (0, 3):
+            raise ValueError(
+                "Cannot specify --removek2 option with other --kmintype "
+                "options")
+        args.kmin_type = "min"
+        args.kmin = 3
+
+    print(args)
+
+    return parser, args
 
 
-def create_page(name_matrix, matrix, kmax, ybound=[0., 1.], ignore_k2=False):
+def create_page(name_matrix, matrix, kmax, ybound=[0., 1.],
+                kmin_type="none", kmin=0, max_avg_pts=0):
     """
     """
     # Create a figure instance (ie. a new page)
@@ -136,17 +171,28 @@ def create_page(name_matrix, matrix, kmax, ybound=[0., 1.], ignore_k2=False):
     ax2.table(cellText=table, loc="center")
     ax2.axis("off")
 
-    if ignore_k2 and kopt == 2:
+    kmin_v = 2
+    if kmin_type == "min" and kmin >= 2:
+        kmin_v = kmin
+    elif kmin_type == "max_avg_pts" and max_avg_pts > 0:
+        kmin_v = matrix.shape[0] / max_avg_pts
+    print('kmin:', kmin_v)
+
+    if kopt < kmin_v:
         del dict_clusters[kopt]
-        k_opt = max(dict_clusters.values())
-        kopt = dict_clusters.keys()[dict_clusters.values().index(k_opt)]
+        k_opt = max([v for k, v in six.iteritems(dict_clusters)
+                     if v >= kmin_v])
+        kopt = dict_clusters.keys()[
+            dict_clusters.values().index(k_opt)]
         ax3.text(0., 0.,
-                 "The optimal number of clusters is " + str(kopt) + ".\n (You "
-                 " have decided to ignore Kopt=2 clusters.)",
+                 "The optimal number of clusters is " + str(kopt)
+                 + ".\n (You have decided to ignore Kopt < %d "
+                 "clusters.)" % kmin_v,
                  color="red")
     else:
         ax3.text(0., 0.,
-                 "The optimal number of clusters is " + str(kopt) + ".",
+                 "The optimal number of clusters is " + str(kopt)
+                 + ".",
                  color="red")
     ax3.axis("off")
 
@@ -159,24 +205,34 @@ def create_page(name_matrix, matrix, kmax, ybound=[0., 1.], ignore_k2=False):
 def main():
     """
     """
-    if len(sys.argv[:]) == 6:
-        # load the arguments of parser (delete script name: sys.arg[0])
-        arguments = (json.dumps(eval(sys.argv[1])),
-                     sys.argv[2],
-                     sys.argv[3],
-                     sys.argv[4],
-                     sys.argv[5])
-    elif len(sys.argv[:]) == 5:
-        # load the arguments of parser (delete script name: sys.arg[0])
-        arguments = (json.dumps(eval(sys.argv[1])),
-                     sys.argv[2],
-                     sys.argv[3],
-                     sys.argv[4])
-    else:
-        # not autoscale Y
-        arguments = (json.dumps(eval(sys.argv[1])),
-                     sys.argv[2],
-                     sys.argv[3])
+    # load the arguments of parser (delete script name: sys.arg[0])
+    #if len(sys.argv) < 2:
+      #parse_args(("-h", ))
+      #sys.exit(0)
+
+    arguments = sys.argv[1:]
+    #if sys.argv[1].startswith("["):
+        #arg0 = json.dumps(eval(sys.argv[1]))
+    #else:
+        #arg0 = sys.argv[1]
+    #arguments = (arg0, ) + tuple(sys.argv[2:])
+    #if len(sys.argv[:]) == 6:
+        #arguments = (json.dumps(eval(sys.argv[1])),
+                     #sys.argv[2],
+                     #sys.argv[3],
+                     #sys.argv[4],
+                     #sys.argv[5])
+    #elif len(sys.argv[:]) == 5:
+        ## load the arguments of parser (delete script name: sys.arg[0])
+        #arguments = (json.dumps(eval(sys.argv[1])),
+                     #sys.argv[2],
+                     #sys.argv[3],
+                     #sys.argv[4])
+    #else:
+        ## not autoscale Y
+        #arguments = (json.dumps(eval(sys.argv[1])),
+                     #sys.argv[2],
+                     #sys.argv[3])
     parser, args = parse_args(arguments)
 
     # the PDF document
@@ -191,7 +247,9 @@ def main():
                                     rmat,
                                     args.kmax,
                                     args.ybound,
-                                    args.ignore_k2)
+                                    args.kmin_type,
+                                    args.kmin,
+                                    args.max_avg_pts)
         # Done with the page
         pp.savefig()
         if args.save_scores:
