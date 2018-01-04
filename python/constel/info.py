10da372ad21eff484f00a93f9fdf666747c900a5
denisri <denis.riviere.gm@gmail.com>
2018-01-04 17:55:18 +0000

    merge + set version 1.3
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/constellation/constellation-private/trunk@80327 613412ce-1361-4b7a-93bf-a3c75af1bae4

---
+++ python/constel/info.py
@@ -0,0 +1,47 @@
+version_major = 1
+version_minor = 3
+version_micro = 0
+version_extra = ''
+
+# Format expected by setup.py and doc/source/conf.py: string of form "X.Y.Z"
+__version__ = "%s.%s.%s%s" % (version_major,
+                              version_minor,
+                              version_micro,
+                              version_extra)
+
+description = 'Constellation, Connectivity-based parcellation of the cortex'
+
+long_description = """
+=============
+CONSTELLATION
+=============
+
+Connectivity-based parcellation of the cortex.
+
+"""
+
+# versions for dependencies
+SPHINX_MIN_VERSION = '1.0'
+
+# Main setup parameters
+NAME = 'constellation-private'
+PROJECT = 'constellation'
+ORGANISATION = "CEA"
+MAINTAINER = "CEA"
+MAINTAINER_EMAIL = ""
+DESCRIPTION = description
+LONG_DESCRIPTION = long_description
+URL = ""
+DOWNLOAD_URL = ""
+LICENSE = ""
+#CLASSIFIERS = CLASSIFIERS
+AUTHOR = "CEA"
+AUTHOR_EMAIL = ""
+PLATFORMS = "Linux, Windows, Mac"
+ISRELEASE = version_extra
+VERSION = __version__
+PROVIDES = ["constellation-gpl"]
+REQUIRES = ["aims-free"]
+EXTRAS_REQUIRE = {
+    "doc": ["sphinx>=%s" % SPHINX_MIN_VERSION],
+}
