f8c55ccd94c6d13872860b56056680020eae1047
Denis Rivière <denis.riviere.gm@gmail.com>
2021-09-09 19:04:51 +0200

    updated project name

--- python/constel/info.py
+++ python/constel/info.py
@@ -24,7 +24,7 @@ Connectivity-based parcellation of the cortex.
 SPHINX_MIN_VERSION = '1.0'
 
 # Main setup parameters
-NAME = 'constellation-private'
+NAME = 'constellation-nonfree'
 PROJECT = 'constellation'
 ORGANISATION = "CEA"
 MAINTAINER = "CEA"
