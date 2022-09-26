46bcbbfce1b1f42f57cc68a6cf9ab1bc2c31e80d
clanglet <clement.langlet@cea.fr>
2022-09-26 11:42:01 +0200

    FibersWeightsReader first commit

---
+++ src/library/fibersWeightsReader.cc
@@ -0,0 +1,98 @@
+#include <constellation/fibersWeightsReader.h>
+
+using namespace std;
+using namespace carto;
+using namespace aims;
+
+namespace constel {
+
+  //---------------------------------------------------------------------------
+  FibersWeightsReader::FibersWeightsReader() :
+    _verbose(false) {}
+
+  //---------------------------------------------------------------------------
+  FibersWeightsReader::FibersWeightsReader(
+    string weightsFilename,  bool verbose)
+    : _weightsFilename(weightsFilename),
+      _verbose(verbose) {}
+
+
+  //---------------------------------------------------------------------------
+  FibersWeightsReader::~FibersWeightsReader() {}
+
+
+  //---------------------------------------------------------------------------
+  void FibersWeightsReader::bundleStarted(
+      const BundleProducer &, const BundleInfo &bundleInfo) {
+    // Open weights file
+    _weightsFile.open( _weightsFilename );
+    if ( _weightsFile.fail() ) {
+      if ( _verbose ) {
+        cout << "Problem reading file: " << _weightsFilename << endl;
+      }
+      exit(1);
+    }
+    else {
+      if ( _verbose ) {
+        cout << "Opened file: " << _weightsFilename << endl;
+      }
+    }
+
+    startBundle(bundleInfo); // needed?
+  }
+
+  //---------------------------------------------------------------------------
+  void FibersWeightsReader::bundleTerminated(
+      const BundleProducer &, const BundleInfo &bundleInfo) {
+    if (bundleInfo.name() == "255")
+      cout << "FibersWeightsReader, bundle 255 found, id: "
+      << bundleInfo.id() << endl;
+    terminateBundle(bundleInfo);
+  }
+
+
+  //---------------------------------------------------------------------------
+  void FibersWeightsReader::fiberStarted(
+      const BundleProducer &, const BundleInfo & /* bundleInfo */,
+      const FiberInfo &fiberInfo /* fiberInfo */) {
+    if ( _verbose ) {
+      cout << "Begin fiber id: " << fiberInfo.id() << endl;
+    }
+
+    // _fiber.clear();
+  }
+
+
+  //---------------------------------------------------------------------------
+  void FibersWeightsReader::fiberTerminated( const BundleProducer &,
+                                     const BundleInfo &bundleInfo,
+                                     const FiberInfo &fiberInfo ) {
+      string weight;
+      getline(_weightsFile, weight, ' ' );
+      fiberInfo.setWeight() = stof(weight);
+
+      if ( _verbose ) {
+        cout << "[FibersWeightsReader] Ending fiber terminated (id, weight): "
+        << fiberInfo.id() << " , " << weight << endl;
+      }
+
+      terminateFiber(bundleInfo, fiberInfo);
+  }
+
+
+  //---------------------------------------------------------------------------
+  void FibersWeightsReader::newFiberPoint(
+      const BundleProducer &, const BundleInfo & /* bundleInfo */,
+      const FiberInfo & /* fiberInfo */, const FiberPoint &point) {}
+
+
+  //---------------------------------------------------------------------------
+  void FibersWeightsReader::noMoreBundle(const BundleProducer &) {
+      _weightsFile.close();
+      BundleProducer::noMoreBundle();
+      if (_verbose) cout << "end of FibersWeightsReader filtering."
+        << endl << flush;
+  }
+
+
+}
---
+++ src/library/fibersWeightsReader.h
@@ -0,0 +1,46 @@
+#ifndef CONSTELLATION_FIBERSWEIGHTSREADER_H
+#define CONSTELLATION_FIBERSWEIGHTSREADER_H
+
+#include <aims/fibers/bundles.h>
+
+
+namespace constel {
+
+  //---------------------------
+  //  FibersWeightsReader
+  //---------------------------
+  class FibersWeightsReader
+    : public aims::BundleProducer, public aims::BundleListener {
+
+   public:
+    FibersWeightsReader(std::string weightsFilename,
+                        bool verbose = true);
+    FibersWeightsReader();
+    virtual ~FibersWeightsReader();
+
+   protected:
+    virtual void bundleStarted(const aims::BundleProducer &,
+                               const aims::BundleInfo &bundleInfo);
+    virtual void bundleTerminated(const aims::BundleProducer &,
+                                  const aims::BundleInfo &bundleInfo);
+    virtual void fiberStarted(const aims::BundleProducer &,
+                              const aims::BundleInfo &,
+                              const aims::FiberInfo &);
+    virtual void fiberTerminated(const aims::BundleProducer &,
+                                 const aims::BundleInfo &bundleInfo,
+                                 const aims::FiberInfo &fiberInfo);
+    virtual void newFiberPoint(const aims::BundleProducer &,
+                               const aims::BundleInfo &,
+                               const aims::FiberInfo &,
+                               const aims::FiberPoint &);
+    virtual void noMoreBundle(const aims::BundleProducer &);
+
+  private:
+    bool _verbose;
+    std::string _weightsFilename;
+    std::ifstream _weightsFile;
+  };
+
+} // namespace constel
+
+#endif // ifndef CONSTELLATION_FIBERSWEIGHTSLISTENER_H
